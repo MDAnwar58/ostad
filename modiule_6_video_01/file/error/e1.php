f75cb3521a7097d07cf5cd340dbb8d7ba91134d8
MD Anwar Sayeed <anwar.saeed656@gmail.com>
2023-03-11 21:00:18 +0600

    Ostad modiule 06

---
+++ modiule_6_video_01/file/error/e1.php
@@ -0,0 +1,8 @@
+<?php
+// $book = 12;
+// error_reporting(E_WARNING | E_NOTICE);
+ini_set("display_errors", 0);
+ini_set("display_startup_errors", 0);
+echo 12/0;
+echo $book;
+echo get_cfg_var("cfg_file_path");
